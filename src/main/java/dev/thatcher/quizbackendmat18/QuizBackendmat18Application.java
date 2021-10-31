b37abe58ad03194c89140f6a76a96a5515f180c2
MAThatcher <MitchellAThatcher96@gmail.com>
2021-10-31 15:30:46 -0500

    init spring boot
    init spring boot

---
+++ src/main/java/dev/thatcher/quizbackendmat18/QuizBackendmat18Application.java
@@ -0,0 +1,13 @@
+package dev.thatcher.quizbackendmat18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBackendmat18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBackendmat18Application.class, args);
+	}
+
+}
